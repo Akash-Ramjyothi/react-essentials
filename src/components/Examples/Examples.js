82577a0d7f27bee74ca89201625ddc8b34dcf273
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2024-08-09 00:25:01 +0530

    Completed React Essentials page

--- src/components/Examples/Examples.js
+++ src/components/Examples/Examples.js
@@ -30,6 +30,7 @@ export default function Examples() {
   return (
     <Section id="examples" title="Examples">
       <Tabs
+        ButtonContainer="menu"
         buttons={
           <>
             <TabComponent
